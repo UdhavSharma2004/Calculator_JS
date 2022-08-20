c63ff50005f5eab09967b833b9a90658256365d3
Udhav Sharma <96074679+UdhavSharma2004@users.noreply.github.com>
2022-08-20 14:35:37 +0530

    calculator Prepared

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-function creator(value){
-
-}
+let obj=document.getElementById("display")
+    let count=0;
+    let ans;
+    function pro(value){
+        if(obj.innerText=="0"){
+            if(value=='a'){
+                ans=0;
+                obj.innerText=0;
+                return;
+            }
+            if(value=='z'){
+                obj.innerText+=0;
+                obj.innerText+=0;
+                ans+=00;
+                return;
+            }
+            ans=value;
+            obj.innerText=value;
+            return;
+        }
+        if(value=='a'){
+            ans=0
+            obj.innerText=0;
+            return;
+        }
+        if(value=='c'){
+            obj.innerText=obj.innerText.slice(0, obj.innerText.length-1)
+            ans=obj.innerText
+            return;
+        }
+        if(value=='z'){
+            obj.innerText+=0;
+            obj.innerText+=0;
+            ans+=00;
+            return;
+        }
+        ans+=value
+        obj.innerText+=value;
+    }
+    function equal(){
+        ans=obj.innerText
+       obj.innerText= eval(ans)
+    }
